b580c2b62f2752d2f353a134a7c796d245b354a1
Ajaykumar21491A1289 <ajju1677143@gmail.com>
2024-06-01 16:48:22 +0530

    first commit

--- provider.tf
+++ provider.tf
@@ -2,8 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.52.0"
+      version = "5.31.0" # AWS provider version, not terraform version
     }
+  }
+
+  backend "s3" {
+    bucket = "ajay-remote-state"
+    key    = "workspace"
+    region = "us-east-1"
+    dynamodb_table = "ajay-locking"
   }
 }
 
