bb6e2c21928263a9de9b3c516336ab7183dec4af
Ajaykumar21491A1289 <ajju1677143@gmail.com>
2024-06-02 11:45:56 +0530

    vpc-Ig-subnets

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "roboshop"{
-    source = "../terraform-vpc"
-    project_name = var.project_name
-    environment  = var.environment
-    common_tags = var.common_tags
-    vpc_tags = var.vpc_tags
+module "roboshop" {
+  source       = "../terraform-vpc"
+  project_name = var.project_name
+  environment  = var.environment
+  common_tags  = var.common_tags
+  vpc_tags     = var.vpc_tags
 
-    public_subnet_cidr = var.public_subnet_cidr
-    private_subnet_cidr = var.public_subnet_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
 }
